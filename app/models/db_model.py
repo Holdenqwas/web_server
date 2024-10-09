81507ab821e6ae33dbffab2b07a6edb0d8105be3
Anton Khorn <anton.pres@yandex.ru>
2024-10-09 23:03:44 +0300

    удалил таблицы

--- app/models/db_model.py
+++ app/models/db_model.py
@@ -30,102 +30,6 @@ def repr_table(table) -> str:
     return output
 
 
-class Training(Base):
-    __tablename__ = "training"
-
-    uid: Mapped[uuid.UUID] = mapped_column(
-        types.Uuid, primary_key=True, server_default=text("gen_random_uuid()")
-    )
-    weight = Column(Float, nullable=True)
-    leg_uid = Column(types.Uuid, ForeignKey("leg.uid"), nullable=True)
-    beak_uid = Column(types.Uuid, ForeignKey("beak.uid"), nullable=True)
-    breast_uid = Column(types.Uuid, ForeignKey("breast.uid"), nullable=True)
-    date = Column(
-        types.DateTime(timezone=True),
-        server_default=func.now(),
-        server_onupdate=func.now(),
-    )
-    name_training = Column(String, nullable=True)
-
-    breast = relationship("Breast", back_populates="training")
-    beak = relationship("Beak", back_populates="training")
-    leg = relationship("Leg", back_populates="training")
-
-    def __repr__(self) -> str:
-        return repr_table(self)
-
-
-class Breast(Base):
-    __tablename__ = "breast"
-
-    uid: Mapped[uuid.UUID] = mapped_column(
-        types.Uuid, primary_key=True, server_default=text("gen_random_uuid()")
-    )
-    bench_press = Column(Float, nullable=True)
-    bench_press_inclined = Column(Float, nullable=True)
-    pullover = Column(Float, nullable=True)
-    dumbbells = Column(Float, nullable=True)
-    block = Column(Float, nullable=True)
-    date = Column(
-        types.DateTime(timezone=True),
-        server_default=func.now(),
-        server_onupdate=func.now(),
-    )
-
-    training = relationship("Training", back_populates="breast")
-
-    def __repr__(self) -> str:
-        return repr_table(self)
-
-
-class Beak(Base):
-    __tablename__ = "beak"
-
-    uid: Mapped[uuid.UUID] = mapped_column(
-        types.Uuid, primary_key=True, server_default=text("gen_random_uuid()")
-    )
-    triceps_bench_press = Column(Float, nullable=True)
-    french_bench_press = Column(Float, nullable=True)
-    kettlebell = Column(Float, nullable=True)
-    gravitron = Column(Float, nullable=True)
-    upper_block = Column(Float, nullable=True)
-    lower_block = Column(Float, nullable=True)
-    date = Column(
-        types.DateTime(timezone=True),
-        server_default=func.now(),
-        server_onupdate=func.now(),
-    )
-
-    training = relationship("Training", back_populates="beak")
-
-    def __repr__(self) -> str:
-        return repr_table(self)
-
-
-class Leg(Base):
-    __tablename__ = "leg"
-
-    uid: Mapped[uuid.UUID] = mapped_column(
-        types.Uuid, primary_key=True, server_default=text("gen_random_uuid()")
-    )
-    army_bench_press = Column(Float, nullable=True)
-    craving_for_chin = Column(Float, nullable=True)
-    head_press = Column(Float, nullable=True)
-    knee_bend = Column(Float, nullable=True)
-    platform_press = Column(Float, nullable=True)
-    extension_of_block = Column(Float, nullable=True)
-    date = Column(
-        types.DateTime(timezone=True),
-        server_default=func.now(),
-        server_onupdate=func.now(),
-    )
-
-    training = relationship("Training", back_populates="leg")
-
-    def __repr__(self) -> str:
-        return repr_table(self)
-
-
 class TrainingAll(Base):
     __tablename__ = "training_all"
 
@@ -319,13 +223,7 @@ class Users(Base):
 
 
 def get_model(name: str):
-    if name == "breast":
-        return Breast
-    elif name == "beak":
-        return Beak
-    elif name == "leg":
-        return Leg
-    elif name == "train1":
+    if name == "train1":
         return Train1
     elif name == "train2":
         return Train2
