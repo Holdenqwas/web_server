fe08f61eeb337222f42def9f818b99d4ce1723e1
Anton Khorn <anton.pres@yandex.ru>
2024-11-24 17:55:32 +0300

    add sert

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 import uvicorn
 from dotenv import load_dotenv
+
 load_dotenv(".env/all.env")
 
 from fastapi import FastAPI
@@ -13,13 +14,20 @@ app = FastAPI()
 app.include_router(router)
 
 # origins = ["http:/localhost:3000", "*"]
-app.add_middleware(CORSMiddleware,
-                   allow_origins=["*"],
-                   # allow_credential=True,
-                   allow_methods=["*"],
-                   allow_headers=["*"])
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    # allow_credential=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host=os.getenv("HOST"), port=int(os.getenv("PORT")))
+    uvicorn.run(
+        "main:app",
+        host=os.getenv("HOST"),
+        port=int(os.getenv("PORT")),
+        ssl_keyfile=os.getenv("SSL_KEYFILE_PATH"),
+        ssl_certfile=os.getenv("SSL_CERTFILE_PATH")
+    )
